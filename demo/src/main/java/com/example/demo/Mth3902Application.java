a2a4dbfd612538a00e1251cfdee116e6c96e5581
Can Koray Karsan <108725475+koraykarsan@users.noreply.github.com>
2024-05-21 15:57:34 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Mth3902Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mth3902Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mth3902Application.class, args);
+	}
+
+}
